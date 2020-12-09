c0ced818c5836757705938524fd55899b841b29a
Yhoungdev <yhoungdev@gmail.com>
2020-12-09 03:13:44 -0800

    push changes

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var typed = new Typed('.typed', {
+  strings: [
+    'UI/UX DESIGNER.',
+    'GRAPHICS DESIGNER.',
+    '2D-GAME DEVELOPER.',
+    'WEB DEVELOPER.'
+  ],
+  typeSpeed: 40,
+  backSpeed: 40,
+  loop: true
+})
